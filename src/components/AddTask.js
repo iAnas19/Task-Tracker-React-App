03fa4cb2bd84efd74566b1b9e31d9cf2202763a8
Anas <anasarif216@gmail.com>
2021-04-27 03:45:33 +0500

    Task info is required's feature is added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -10,23 +10,24 @@ const AddTask = ( { onAdd }) => {
 
     const onSubmit = (e) => {
         e.preventDefault()
+    
 
-        if(!text) {
-            alert('pleae add a Task')
+        if (!text) {
+            alert('pleae add a task')
             return
 
         }
 
-        onAdd({text, day, reminder})
+        onAdd({ text, day, reminder })
 
 
-        setText(' ')
-        setDay(' ')
+        setText('')
+        setDay('')
         setReminder(false)
     }
 
     return (
-        <form className = 'add-form' onSubmit={onSubmit}>
+        <form className = 'add-form' onSubmit ={onSubmit} >
             <div className='form-control'>
                 <label>Task</label>
                 <input type='text' placeholder='Add Task' value = {text} onChange={(e) => setText(e.target.value)} />

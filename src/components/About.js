bfd4b3cbd7bb1d5849c5cd516afb7a6d6ec676e7
Anas <anasarif216@gmail.com>
2021-05-11 03:18:16 +0500

    presentation

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div>
+    <div className='aboutsec'>
       <h4>Version 1.0.0</h4>
-      <Link to='/'>Go Back</Link>
+      <Link to='/' style={{ textDecoration: 'none' }}>Go Back</Link>
     </div>
   )
 }
